b7007a78bc1c36754a723e97865e148ef0e1f21f
Petr <petr@duck.com>
2024-06-08 17:14:34 +0300

    xrdp session added

--- rollout.sh
+++ rollout.sh
@@ -50,18 +50,18 @@ fi
 if [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
 	# 2. update package manager
 	sudo apt update && sudo apt upgrade -y
-	sudo apt install -y apache2 python3-{pip,venv} ssh neovim git fish tmux wget aria2 curl rsync vsftpd zsh && sudo systemctl enable apache2
+	sudo apt install -y apache2 python3-{pip,venv} ssh neovim git fish tmux wget aria2 curl rsync vsftpd xfce4 xrdp zsh && sudo systemctl enable apache2
 elif [ "$ID" = "fedora" ]; then
 	# exact packages are not tested! specified here as conditional example! everything was tested on ubuntu VM only!
 	dnf check-update
-	sudo dnf install -y apache2 python3 neovim git fish openssh-server tmux wget aria2 curl rsync vsftpd zsh && sudo systemctl enable --now sshd && sudo systemctl enable apache2
+	sudo dnf install -y apache2 python3 neovim git fish openssh-server tmux wget aria2 curl rsync vsftpd xfce4 xrdp zsh && sudo systemctl enable --now sshd && sudo systemctl enable apache2
 elif [ "$ID" = "opensuse" ]; then
 	# exact packages are not tested! specified here as conditional example! everything was tested on ubuntu VM only!
 	sudo zypper refresh
-	sudo zypper install apache2 python neovim git fish openssh tmux wget aria2 curl rsync vsftpd zsh && sudo systemctl enable --now sshd.service && sudo systemctl enable apache2
+	sudo zypper install apache2 python neovim git fish openssh tmux wget aria2 curl rsync vsftpd xfce4 xrdp zsh && sudo systemctl enable --now sshd.service && sudo systemctl enable apache2
 elif [ "$ID_LIKE" = "arch" ]; then
 	# exact packages are not tested! specified here as conditional example! everything was tested on ubuntu VM only!
-	sudo pacman -Syu apache python neovim git fish openssh tmux wget aria2 curl rsync vsftpd zsh && sudo systemctl enable --now sshd && sudo systemctl enable apache2
+	sudo pacman -Syu apache python neovim git fish openssh tmux wget aria2 curl rsync vsftpd xfce4 xrdp zsh && sudo systemctl enable --now sshd && sudo systemctl enable apache2
 fi
 
 # generate key if not exists
@@ -142,3 +142,6 @@ EOF
 sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.orig
 sudo mv $HOME/vsftpd.conf /etc/vsftpd.conf
 sudo systemctl enable --now vsftpd
+
+# xrdp config
+echo xfce4-session >~/.xsession
