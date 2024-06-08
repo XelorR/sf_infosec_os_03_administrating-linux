2cca3e7222a0146fd71dc0fab2ddca0e51b87f4c
Petr <petr@duck.com>
2024-06-08 18:17:16 +0300

    some issues in ftp config fixed but it still doesn't work by some reason

--- rollout.sh
+++ rollout.sh
@@ -118,26 +118,26 @@ rm -rf $archiveName
 
 # enabling ftp
 cat <<EOF >$HOME/vsftpd.conf
-anonymous_enable = NO
-local_enable = YES
-write_enable = YES
-local_umask = 022
-xferlog_enable = YES
-xferlog_std_format=YES
-connect_from_port_20 = YES
-chroot_local_user = YES
-allow_writeable_chroot = YES
-ssl_enable=YES
-ssl_tlsv1=YES
-ssl_sslv2=NO
-ssl_sslv3=NO
-rsa_cert_file=/etc/ssl/private/ssl-cert-snakeoil.pem
-rsa_private_key_file=/etc/sssl_ciphers=HIGHsl/private/ssl-cert-snakeoil.key
 allow_anon_ssl=NO
+anonymous_enable=NO
+allow_writeable_chroot=YES
+chroot_local_user=YES
+connect_from_port_20=YES
 force_local_data_ssl=YES
 force_local_logins_ssl=YES
+local_enable=YES
+local_umask=022
+rsa_cert_file=/etc/ssl/private/ssl-cert-snakeoil.pem
+rsa_private_key_file=/etc/ssl/private/ssl-cert-snakeoil.key
+ssl_enable=YES
 ssl_ciphers=HIGH
+ssl_sslv2=NO
+ssl_sslv3=NO
+ssl_tlsv1=YES
 use_localtime=YES
+write_enable=YES
+xferlog_enable=YES
+xferlog_std_format=YES
 EOF
 sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.orig
 sudo mv $HOME/vsftpd.conf /etc/vsftpd.conf
