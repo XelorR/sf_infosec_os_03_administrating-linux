bdeac3450dc35c4a238bc12fc47ddbe4429792b0
Petr <petr@duck.com>
2024-06-19 08:43:30 +0300

    running heredoc with sudo fixed

--- baseline/backup.sh
+++ baseline/backup.sh
@@ -2,14 +2,13 @@
 
 # create backup script
 sudo mkdir -p /archive
-sudo cat <<'EOF' >/archive/backup.sh
+sudo sh -c 'cat << EOF > /archive/backup.sh
 #!/bin/bash
 
 cd /
 tar cpf "/archive/backup-\$(date +%Y-%m-%d_%H%M%S).tar" --exclude="\$HOME/.*" /etc/ssh/sshd_config /etc/xrdp /etc/vsftpd.conf /etc/ssh/sshd_config /var/log "\$HOME"
-EOF
+EOF'
 sudo chmod +x /archive/backup.sh
-sudo mkdir -p /archive
 
 # create cron job
 if ! sudo crontab -l | grep -q '30 18 \* \* 5 /archive/backup.sh'; then
